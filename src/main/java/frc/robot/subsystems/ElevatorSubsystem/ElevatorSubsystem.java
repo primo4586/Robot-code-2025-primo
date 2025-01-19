54f9a969317f75d485ea93cb41ba5688423b47e5
shay k <shaykalisher7@gmail.com>
2025-01-19 23:04:45 +0200

    subsystem not done and commands

---
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem/ElevatorSubsystem.java
@@ -0,0 +1,73 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.ElevatorSubsystem.ElevatorConstants;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.ControlModeValue;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class ElevatorSubsystem extends SubsystemBase implements ElevatorConstants {
+  private TalonFX m_primaryTalonFX;
+  private TalonFX m_followTalonFX;
+  private DigitalInput digitalSwitch;
+  private static ElevatorSubsystem instance;
+  /** Creates a new ElevatorSubsystem. */
+
+
+  //instance:
+  public static ElevatorSubsystem getInstance(){
+    if (instance == null)
+      instance = new ElevatorSubsystem();
+    return instance;
+  }
+
+  //TODO: set follow motor to  actually follow
+
+  private ElevatorSubsystem()
+   {
+    configs();
+    //initing:
+    m_primaryTalonFX = new TalonFX(PRIMARY_TALONFX_ID, CAN_BUS_NAME);
+    m_primaryTalonFX = new TalonFX(FOLLOW_TALONFX_ID, CAN_BUS_NAME);
+    digitalSwitch = new DigitalInput(DIGITAL_SWITCH_ID);
+  }
+
+  
+
+  //reseting pos
+  public Command resetPosition(){
+    return run(()-> {
+    m_primaryTalonFX.setPosition(0);
+    m_followTalonFX.setPosition(0);
+  });
+  }
+
+
+  //hCommand -  home command, takes target position and moves to that position
+  public Command hCommand(double targetPos){
+    return run(()-> {
+      if(digitalSwitch.get()){ //if difital switch is being pressed
+        m_primaryTalonFX.setPosition(0);  
+
+        //TODO: add follow and then u dont need this shit because it follows
+        m_followTalonFX.setPosition(targetPos); 
+    }
+    });
+  }
+
+  
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+  private void configs(){
+
+  }
+}

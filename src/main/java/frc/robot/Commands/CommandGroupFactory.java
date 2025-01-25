7b9742f92fb7fc176db49f4855008ebf622c8a4d
Eilon huppert <eilonhuppert@gmail.com>
2025-01-25 15:34:29 +0200

    started drive to reef

--- src/main/java/frc/robot/Commands/CommandGroupFactory.java
+++ src/main/java/frc/robot/Commands/CommandGroupFactory.java
@@ -13,6 +13,8 @@ import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
+import static frc.robot.Misc.*;
+
 public class CommandGroupFactory {
 
     private static final CommandSwerveDrivetrain swerve = RobotContainer.drivetrain;
@@ -20,7 +22,9 @@ public class CommandGroupFactory {
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
 
-
+        // public static Command driveToReefCommand(boolean isRight){
+        //         return RunCommand(() -> swerve.setControl)
+        // }
 /**
  * Creates a command to align the robot's orientation towards a specified target point.
  * This command uses a PID controller to adjust the robot's rotational rate
@@ -31,23 +35,18 @@ public class CommandGroupFactory {
  * @param targetPoint The 2D translation point that the robot should align to.
  * @return A command that aligns the robot's heading towards the target point.
  */
-       public static Command alignPointCommand(Translation2d targetPoint) { // todo connect cameras
+       public static Command alignPointCommand() { // todo connect cameras
         // todo add all 8 reef 2d points. 
 
                 PIDController pidController = new PIDController(0.0, 0.0, 0); // todo tune
                 pidController.enableContinuousInput(-180, 180);
                 pidController.setTolerance(0.5);
                 pidController.setSetpoint(0);
-
                 return new RunCommand(
                                 () -> swerve.setControl(
                                                 forwardStraight.withRotationalRate(
-                                                                pidController.calculate(
-                                                                                swerve.getState().Pose.getRotation() // todo: switch with camera
-                                                                                                .getDegrees(),
-                                                                                calculateAngleToPoint(targetPoint)
-                                                                                                .getDegrees())))); // if Tolerance dose not work 
-                                                                                                //,swerve).until(() -> pidController.atSetpoint()).andThen(() -> pidController.close());
+                                                                pidController.calculate(5)))).// todo: connect to camera
+                                                                        until(() -> pidController.atSetpoint());
         }
 
         /**
@@ -92,5 +91,49 @@ public class CommandGroupFactory {
         //         return new Rotation2d(angleToTargetRadians + Units.degreesToRadians(180));
         // }
 
-    
+        public static Pose2d ChooseReef(boolean isRight){ 
+
+                Pose2d currentPosition = swerve.getState().Pose;
+                /*
+                 * caculate the angel between the robot and the reef 
+                 * from here we can know to wich side of the reef the robot needs to go.
+                */
+                double reefAngle = Math.toDegrees(Math.atan(currentPosition.getY() - REEF_CENTER_POSITION.getY() /
+                        currentPosition.getX() - REEF_CENTER_POSITION.getX()));
+                reefAngle /= 60; 
+                switch((int)reefAngle){
+                        case 0: return new Pose2d(
+                                isRight ? REEF_A_POSITION.getX() : REEF_B_POSITION.getX(),
+                                isRight ? REEF_A_POSITION.getY() : REEF_B_POSITION.getY(),
+                                new Rotation2d(REEF_1_ANGEL)
+                        );
+                        case 1: return new Pose2d(
+                                isRight ? REEF_C_POSITION.getX() : REEF_D_POSITION.getX(),
+                                isRight ? REEF_C_POSITION.getY() : REEF_D_POSITION.getY(),
+                                new Rotation2d(REEF_2_ANGEL)
+                        );
+                        case 2: return new Pose2d(
+                                isRight ? REEF_E_POSITION.getX() : REEF_F_POSITION.getX(),
+                                isRight ? REEF_E_POSITION.getY() : REEF_F_POSITION.getY(),
+                                new Rotation2d(REEF_3_ANGEL)
+                        );
+                        case 3: return new Pose2d(
+                                isRight ? REEF_G_POSITION.getX() : REEF_H_POSITION.getX(),
+                                isRight ? REEF_G_POSITION.getY() : REEF_H_POSITION.getY(),
+                                new Rotation2d(REEF_4_ANGEL)
+                        );
+                        case 4: return new Pose2d(
+                                isRight ? REEF_I_POSITION.getX() : REEF_J_POSITION.getX(),
+                                isRight ? REEF_I_POSITION.getY() : REEF_J_POSITION.getY(),
+                                new Rotation2d(REEF_5_ANGEL)
+                        );
+                        case 5: return new Pose2d(
+                                isRight ? REEF_K_POSITION.getX() : REEF_L_POSITION.getX(),
+                                isRight ? REEF_K_POSITION.getY() : REEF_L_POSITION.getY(),
+                                new Rotation2d(REEF_6_ANGEL)
+                        );
+                        default: return new Pose2d();
+                }
+        
+        }
 }

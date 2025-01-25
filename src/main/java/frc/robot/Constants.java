87648d9065243b1aeb28705a64764366dfcb8987
shay k <shaykalisher7@gmail.com>
2025-01-25 20:59:10 +0200

    added pid, motion magic and commands

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,12 +2,28 @@ package frc.robot;
 
 public final class Constants {
     public static class Elevator{
-        public static final int PRIMARY_TALONFX_ID = 0;
+        public static final int MASTER_TALONFX_ID = 0;
         public static final int FOLLOW_TALONFX_ID = 1;
         public static final int FIRST_ELEVATOR_INCODER = 2;
         public static final int SECOND_ELEVATOR_INCODER = 3;
         public static final  int DIGITAL_SWITCH_ID = 4;
         public static final  String CAN_BUS_NAME = "canBus";
+
+        public static final double MM_CRUISE = 0;
+        public static final double MM_ACCELERATION = 0;
+        public static final double MM_JERK = 0;
+
+        public static final int kp = 0;
+        public static final int kV = 0;
+        public static final int kP = 0;
+        public static final int kI = 0;
+        public static final int kD = 0;
+        public static final int kS = 0;
+        public static final int kA = 0;
+        public static final int kG = 0;
+
+
+
     }
 
 }
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem/ElevatorSubsystem.java
@@ -3,16 +3,38 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems.ElevatorSubsystem;
-import com.ctre.phoenix6.hardware.TalonFX;
-
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.ControlModeValue;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.sim.TalonFXSimState;
+import com.ctre.phoenix6.controls.ControlRequest;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.signals.ControlModeValue;
+
+import com.ctre.*;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import frc.robot.Constants;
+import frc.robot.Constants.Elevator;
+
+import java.lang.module.Configuration;
+import java.time.Duration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
+
+import com.ctre.phoenix6.controls.*;
 import frc.robot.Constants.Elevator.*;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import com.ctre.phoenix6.controls.*;
 
 
 
@@ -31,17 +53,19 @@ public class ElevatorSubsystem extends SubsystemBase {
     return instance;
   }
 
-  //TODO: set follow motor to  actually follow
 
   private ElevatorSubsystem()
    {
     //configs();
     //initing:
-    m_masterTalonFX = new TalonFX(Constants.Elevator.PRIMARY_TALONFX_ID, Constants.Elevator.CAN_BUS_NAME);
-    m_followTalonFX = new TalonFX(Constants.Elevator.FOLLOW_TALONFX_ID, Constants.Elevator.CAN_BUS_NAME);
-    digitalSwitch = new DigitalInput(Constants.Elevator.DIGITAL_SWITCH_ID);
+    m_masterTalonFX = new TalonFX(Elevator.MASTER_TALONFX_ID);
+    m_followTalonFX = new TalonFX(Elevator.FOLLOW_TALONFX_ID);
+    digitalSwitch = new DigitalInput(Elevator.DIGITAL_SWITCH_ID);
 
-    m_followTalonFX.follow(m_masterTalonFX);  
+    m_followTalonFX.setInverted(false);
+    m_masterTalonFX.setInverted(false);
+    resetPosition();
+    
 
   }
 
@@ -50,31 +74,66 @@ public class ElevatorSubsystem extends SubsystemBase {
   //reseting pos
   public Command resetPosition(){
     return run(()-> {
-      m_masterTalonFX.setPosition(0);
-    m_followTalonFX.setPosition(0);
+      resetPosition();
   });
   }
 
 
-  //hCommand -  home command, takes target position and moves to that position
-  public Command hCommand(double targetPos){
-    return run(()-> {
-      if(digitalSwitch.get()){ //if difital switch is being pressed
-        m_masterTalonFX.setPosition(0);  
-
-        //TODO: add follow and then u dont need this shit because it follows
-        m_followTalonFX.setPosition(targetPos); 
-    }
-    });
+  //hCommand - home command, takes target position and moves to that position
+     // Relocate to a given position using Motion Magic
+  public Command relocatePositionCommand(double targetPos) {
+    return run(() -> {
+          m_masterTalonFX.set(targetPos); 
+          m_followTalonFX.set(targetPos); 
+      });
   }
 
-  
+
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
   private void configs(){
+    TalonFXConfiguration  configuration = new TalonFXConfiguration();
+    MotionMagicConfigs mm = new MotionMagicConfigs();
+    
+    mm.MotionMagicCruiseVelocity = Elevator.MM_CRUISE; //maximun velocity (peak velocity of the motion)
+    mm.MotionMagicAcceleration = Elevator.MM_ACCELERATION; //controls acceleration + deceleration (beginning and end of motion)
+    mm.MotionMagicJerk = Elevator.MM_JERK; //jerk - derivative of acceleration
+
+    configuration.MotionMagic = mm; 
+
+  /*
+    p - proportional gain, controls the system's response to the current error in position
+    i - integral gain, controls the system's response to collected errors over time
+    d - derivative gain, controls how fast the position is changing
+    s - static friction gain - compensates for static friction (ensures that the motor has enough output to initiate motion)
+    v - velocity gain - controls the motor output based on the velocity of the elevator
+    a - acceleration gain - controls the motor output based on the acceleration
+    g - gravity - output to overcome gravity
+   */
+
+    configuration.Slot0.kP = Constants.Elevator.kp;
+    configuration.Slot0.kI = Constants.Elevator.kI;
+    configuration.Slot0.kD = Constants.Elevator.kD;
+    configuration.Slot0.kS = Constants.Elevator.kS;
+    configuration.Slot0.kA = Constants.Elevator.kA;
+    configuration.Slot0.kV = Constants.Elevator.kV;
+    configuration.Slot0.kG = Constants.Elevator.kG;
+
+
+    StatusCode statusCode = StatusCode.StatusCodeNotInitialized;
+    for(int i = 0; i < 5; i++){
+      statusCode = m_masterTalonFX.getConfigurator().apply(configuration);
+      if(statusCode.isOK())
+        break;
+    }
+    if(!statusCode.isOK())
+      System.out.println("master motor cant apply config, error code: " + statusCode.toString());
+
+
+
     
   }
 }

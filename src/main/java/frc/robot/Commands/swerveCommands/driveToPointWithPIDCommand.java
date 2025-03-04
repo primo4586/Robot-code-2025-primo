7dcb1b42aecd363fb35b90ce8daee17f08858deb
Eilon huppert <eilonhuppert@gmail.com>
2025-03-04 09:28:26 +0200

    swerve facing angel (NOT TESTED AND I NEED TO ADD SYSID)

--- src/main/java/frc/robot/Commands/swerveCommands/driveToPointWithPIDCommand.java
+++ src/main/java/frc/robot/Commands/swerveCommands/driveToPointWithPIDCommand.java
@@ -1,8 +1,8 @@
 
-
 package frc.robot.Commands.swerveCommands;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest.FieldCentricFacingAngle;
 
 import java.util.function.DoubleSupplier;
 
@@ -18,30 +18,34 @@ import frc.robot.RobotContainer;
 import frc.robot.PrimoLib.PrimoCalc;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
-
-public class driveToPointWithPIDCommand extends Command { //TODO: need to orgeniz and tune evrything + add comments
+public class driveToPointWithPIDCommand extends Command { // TODO: need to orgeniz and tune evrything + add comments
   private static Pose2d _target;
-                private DoubleSupplier vector = () -> DriverStation.getAlliance().orElse(Alliance.Red) == Alliance.Blue ? 1 : -1;
+  private DoubleSupplier vector = () -> DriverStation.getAlliance().orElse(Alliance.Red) == Alliance.Blue ? 1 : -1;
 
-  static double  MaxSpeed = RobotContainer.MaxSpeed;
-  static double  MaxAngularRate = RobotContainer.MaxSpeed;
+  static double MaxSpeed = RobotContainer.MaxSpeed;
+  static double MaxAngularRate = RobotContainer.MaxSpeed;
   private static final CommandSwerveDrivetrain swerve = RobotContainer.drivetrain;
-  private static final SwerveRequest.FieldCentric roborCentric = new SwerveRequest.FieldCentric() // TODO change to field centric facing angel and tune facing angel with sysid
-  .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-  .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+
+  private static final SwerveRequest.FieldCentricFacingAngle facingAngel = new FieldCentricFacingAngle()
+      .withDeadband(MaxSpeed * 0.1) // ^joy stick deadband but i'm not shure why we need Max Speed here?
+      .withRotationalDeadband(MaxAngularRate * 0.1)
+      .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
   private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
 
-  PIDController driveXPidController = new PIDController(5, 1.5, 0.4);
+  /*
+   * i dont think we need profile because we use feed foword to control the
+   * velocity
+   * and we use FOC to control the acceleration.
+   * but MA did use profile.
+   */
+  PIDController driveXPidController = new PIDController(5, 1.5, 0.4); // TODO: check the values again.
   PIDController driveYPidController = new PIDController(5, 1.5, 0.4);
-  PIDController rotionPidController = new PIDController(0.025, 0, 0.01);
+
   /** Creates a new driveToPointWithPIDCommand. */
   public driveToPointWithPIDCommand(boolean isRight) {
     addRequirements(swerve);
     _target = PrimoCalc.ChooseReef(isRight);
-    rotionPidController.enableContinuousInput(180, -180);
-    rotionPidController.setSetpoint(_target.getRotation().getRadians());
-    rotionPidController.setTolerance(1);
 
     driveXPidController.setSetpoint(_target.getX());
     driveXPidController.setTolerance(0.02);
@@ -49,12 +53,8 @@ public class driveToPointWithPIDCommand extends Command { //TODO: need to orgeni
     driveYPidController.setTolerance(0.02);
   }
 
-  public driveToPointWithPIDCommand(Pose2d target){
+  public driveToPointWithPIDCommand(Pose2d target) {
     _target = target;
-    rotionPidController.enableContinuousInput(180, -180);
-    rotionPidController.setSetpoint(target.getRotation().getDegrees());
-    rotionPidController.setTolerance(1);
-
     driveXPidController.setSetpoint(target.getX());
     driveXPidController.setTolerance(0.02);
     driveYPidController.setSetpoint(target.getY());
@@ -64,7 +64,8 @@ public class driveToPointWithPIDCommand extends Command { //TODO: need to orgeni
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called everey time the scheduler runs while the command is schduled.
   @Override
@@ -72,15 +73,17 @@ public class driveToPointWithPIDCommand extends Command { //TODO: need to orgeni
     SmartDashboard.putNumber("swerve x error", _target.getX() - swerve.getState().Pose.getX());
     SmartDashboard.putNumber("swerve y error", _target.getY() - swerve.getState().Pose.getY());
     swerve.setControl(
-      roborCentric.withVelocityX(  vector.getAsDouble() * driveXPidController.calculate(swerve.getState().Pose.getX()) * 0.7)
-        .withVelocityY(vector.getAsDouble() * driveYPidController.calculate(swerve.getState().Pose.getY()) * 0.7)
-    );
+        facingAngel
+            .withVelocityX(vector.getAsDouble() * driveXPidController.calculate(swerve.getState().Pose.getX()) * 0.7)
+            .withVelocityY(vector.getAsDouble() * driveYPidController.calculate(swerve.getState().Pose.getY()) * 0.7)
+            .withTargetDirection(swerve.getState().Pose.getRotation()));
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    swerve.applyRequest(() -> brake);
+    swerve.applyRequest(() -> brake); // ^ this might be too much and will kill the robot. the pid it self should stop
+                                      // the swerve.
   }
 
   // Returns true when the command should end.

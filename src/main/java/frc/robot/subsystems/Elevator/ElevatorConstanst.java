64d7059c871ec3239676a3dedf54db60e2a8053f
Primo4586 <primo4586@gmail.com>
2025-03-14 12:15:22 +0200

    swerve current limits went down

--- src/main/java/frc/robot/subsystems/Elevator/ElevatorConstanst.java
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorConstanst.java
@@ -55,10 +55,10 @@ public class ElevatorConstanst {
 
     public static final double L1_HEIGHT = 0;
     public static final double L2_HEIGHT = 0.703857421875;
-    public static final double L3_HEIGHT = 1.390380859375;
-    public static final double L4_HEIGHT = 2.441650390625; 
+    public static final double L3_HEIGHT = 1.46826171875;
+    public static final double L4_HEIGHT = 2.517822265625; 
     //our reef height: 2.553173828125 - 0.10;
-    //real reef height: 2.441650390625; 
+    //real reef height: 2.517822265625; 
 
     public static final double LOW_ALGEA_HEIGHT = 0.89;
     public static final double HIGH_ALGEA_HEIGHT = 0;
